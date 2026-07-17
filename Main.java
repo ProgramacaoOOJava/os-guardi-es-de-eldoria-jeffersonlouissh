697066c2272b9bed8f65ef4deb21522db28306b9
Jefferson Louis <137832123+jeffersonlouissh@users.noreply.github.com>
2026-07-17 22:10:55 +0000

    implementado o desafio nivel novato da introducao a programacao em java

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
-
-        .
-        .
-        .
+        Personagem heroi2 = new Personagem("Artemis","Arqueira", 5, 80, 12.5);
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
 
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -7,18 +7,20 @@ public class Personagem {
 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
     public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+        System.out.println("------ Status do Personagem -------");
+        System.out.println("  Nome: " + nome);
+        System.out.println("  Classe: " + classe);
+        System.out.println("  Nivel: " + nivel);
+        System.out.println("  Pontos de Vida: " + pontosDeVida);
+        System.out.println("  Poder Base: " + poderBase);
+        System.out.println("\n");
     }
 }
 
